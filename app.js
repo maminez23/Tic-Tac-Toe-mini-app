24554fb8f2b7b3cec9e4d6a29ed92448f0b61269
maminez23 <60395940+maminez23@users.noreply.github.com>
2020-04-27 19:59:36 +0200

    check function done

--- app.js
+++ app.js
@@ -5,15 +5,53 @@ var count = 0;
 let result = [[],[],[]];
 
 //*******Helpers*************
-function updateResult(ref, text){
-    var col = ref[1];
-    var row = ref[0];
-    console.log(col,row, result)
+function updateResult(col, row, text){
     result[row][col] = text;
+    console.log(result)
 }
 
-function check(start){
+function verifyColumn(col, text) {
+    for(let i = 0; i < 3; i++){
+        if(result[i][col] !== text){
+            return false
+        }
+    }
+    return true
+}
 
+function verifyRow(row, text){
+    for(let i = 0; i < 3; i++){
+        if(result[row][i] !== text){
+            return false
+        }
+    }
+    return true
+}
+function verifyMajorDiago(text) {
+    for(let i = 0; i < 3; i++){
+        if(result[i][i] !== text){
+            return false
+        }
+    }
+    return true
+}
+function verifyMinorDiago(text) {
+    for(let i = 2; i >= 0; i--){
+        for(let j = 0; j < 3; j++){
+            if(result[j][i] !== text){
+                return false
+            }
+        }
+    }
+    return true
+}
+
+function check(col, row, text){
+    console.log('inside check', text);
+    if(verifyColumn(col, text) || verifyRow(row, text)) return true;
+    if(col === row){
+        if(verifyMajorDiago(text) || verifyMinorDiago(text)) return true
+    }
 }
 
 //******   MAIN App **********
@@ -21,15 +59,25 @@ function draw(){
     symbol = (symbol === 'X')? 'O' : 'X';
     this.textContent = symbol;
     // *****this part will return the matrix representation of the game which is the var result
-    var ref = this.id;
-    updateResult(ref, this.textContent);
+    var col = this.id[1];
+    var row = this.id[0]
+    updateResult(col, row, symbol);
    //********
     count++;
     this.removeEventListener("click", draw, false);
     if(count >= 6){
-        check(ref);
+        if(check(col, row, symbol)){
+            if(symbol === 'X'){
+                console.log('Player 1 wins')
+            }
+            else{
+                console.log('Player 2 wins')
+            }
+        }
+        else{
+            console.log('still')
+        }
     }
-
 }
 
 //********Events*********
@@ -41,5 +89,7 @@ for(let i = 0 ; i < tds.length; i++){
 document.getElementsByTagName('button')[0].addEventListener('click', function(){
     for(let i = 0 ; i < tds.length; i++){
         tds[i].textContent = ''
-        }
+    }
+    count = 0;
+    result = [[],[],[]]
 });
